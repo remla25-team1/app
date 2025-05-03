f0291c1f2902fd5f3375c7dea9bcc7b3cb9ab936
Yifan Sun <Y.sun-44@student.tudelft.nl>
2025-05-03 20:27:21 +0200

    fix swagger

--- src/swagger_template.py
+++ src/swagger_template.py
@@ -1,28 +1,12 @@
-def generate_swagger_doc(summary = "", description ="", request_example=None, response_example=None, required_fields=None):
+def generate_swagger_doc(summary="", description="", request_example=None, response_example=None, required_fields=None,has_body=True):
     request_example = request_example or {}
     response_example = response_example or {}
     required_fields = required_fields or []
-    return {
+
+    swagger = {
         "summary": summary,
         "description": description,
         "consumes": ["application/json"],
-        "parameters": [
-            {
-                "name": "input",
-                "in": "body",
-                "required": True,
-                "schema": {
-                    "type": "object",
-                    "required": required_fields,
-                    "properties": {
-                        key: {
-                            "type": "string",
-                            "example": value
-                        } for key, value in request_example.items()
-                    }
-                }
-            }
-        ],
         "responses": {
             200: {
                 "description": "Successful response",
@@ -38,3 +22,24 @@ def generate_swagger_doc(summary = "", description ="", request_example=None, re
             }
         }
     }
+
+    if has_body:
+        swagger["parameters"] = [
+            {
+                "name": "input",
+                "in": "body",
+                "required": True,
+                "schema": {
+                    "type": "object",
+                    "required": required_fields,
+                    "properties": {
+                        key: {
+                            "type": "string",
+                            "example": value
+                        } for key, value in request_example.items()
+                    }
+                }
+            }
+        ]
+
+    return swagger
